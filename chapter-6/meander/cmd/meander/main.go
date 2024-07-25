6a7c8968f9be721ae81dde26deacb6a61d999665
Andres Eloy <andres.epacheco99@gmail.com>
2024-07-24 21:51:53 -0400

    feat: Chapter 6 - Meander app using Google Maps API

---
+++ chapter-6/meander/cmd/meander/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/andrespd99/meander"
+)
+
+func main() {
+
+	meander.APIKey = os.Getenv("GCLOUD_API_KEY")
+
+	http.HandleFunc("/journeys", cors(func(w http.ResponseWriter, r *http.Request) {
+		respond(w, r, meander.Journeys)
+	}))
+	http.HandleFunc("/recommendations", cors(func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		var err error
+		q.Lat, err = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Lng, err = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Radius, err = strconv.Atoi(r.URL.Query().Get("radius"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	}))
+	http.ListenAndServe(":8080", http.DefaultServeMux)
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		f(w, r)
+	}
+}
+
+func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
+	publicData := make([]interface{}, len(data))
+	for i, d := range data {
+		publicData[i] = meander.Public(d)
+	}
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(publicData)
+}
---
+++ chapter-6/meander/go.mod
@@ -0,0 +1,8 @@
+module github.com/andrespd99/meander
+
+go 1.22.4
+
+require (
+	github.com/cheekybits/is v0.0.0-20150225183255-68e9c0620927 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
